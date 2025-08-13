40d474ce22e1512266042e7ec2e44c1bdf1d18f1
Paulo Paiva <paulo.h.paiva.c@gmail.com>
2025-08-13 15:36:24 -0300

    created component category

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CategoryComponent } from '../../features/category/view/category/category.component';
-import { MatDividerModule } from '@angular/material/divider';
 import { TaskComponent } from '../../features/task/view/task/task.component';
+import { MatDividerModule } from '@angular/material/divider';
 const COMPONENTS = [CategoryComponent, TaskComponent];
 const MODULES = [MatDividerModule];
 @Component({
@@ -9,11 +9,10 @@ const MODULES = [MatDividerModule];
   standalone: true,
   imports: [...COMPONENTS, ...MODULES],
   template: ` <div class="h-screen flex w-full">
-    <app-category class="w-1/4 border-2" />
-    <mat-divider class="h-full opacity-50 vertical" />
+    <app-category class="w-1/4" />
+    <mat-divider vertical class="h-full opacity-50" />
     <app-task class="w-3/4" />
   </div>`,
-  styles: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {}
